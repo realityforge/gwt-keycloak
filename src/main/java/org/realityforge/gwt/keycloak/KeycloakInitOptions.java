eddc31250c1268e580ae55f8cfab2dcbc3bcb8bf
Andrea Granados <andreaagranados@gmail.com>
2024-06-26 09:24:17 +1000

    Fixing formatting.

--- src/main/java/org/realityforge/gwt/keycloak/KeycloakInitOptions.java
+++ src/main/java/org/realityforge/gwt/keycloak/KeycloakInitOptions.java
@@ -1,6 +1,10 @@
 package org.realityforge.gwt.keycloak;
 
-import jsinterop.annotations.*;
+import jsinterop.annotations.JsNonNull;
+import jsinterop.annotations.JsNullable;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
 
 /**
  * Options class to pass to init action.
@@ -10,69 +14,70 @@ import jsinterop.annotations.*;
         namespace = JsPackage.GLOBAL,
         name = "Object"
 )
-public class KeycloakInitOptions {
-
-    public KeycloakInitOptions() {
+public class KeycloakInitOptions
+{
+    public KeycloakInitOptions()
+    {
     }
 
-    @JsProperty(name = "onLoad")
+    @JsProperty( name = "onLoad" )
     @JsNullable
     public native String onLoad();
 
     @JsProperty
-    public native void setOnLoad(@JsNullable String onLoad);
+    public native void setOnLoad( @JsNullable String onLoad );
 
-    @JsProperty(name = "token")
+    @JsProperty( name = "token" )
     @JsNullable
     public native String token();
 
     @JsProperty
-    public native void setToken(@JsNullable String token);
+    public native void setToken( @JsNullable String token );
 
-    @JsProperty(name = "refreshToken")
+    @JsProperty( name = "refreshToken" )
     @JsNullable
     public native String refreshToken();
 
     @JsProperty
-    public native void setRefreshToken(@JsNullable String refreshToken);
+    public native void setRefreshToken( @JsNullable String refreshToken );
 
-    @JsProperty(name = "IdToken")
+    @JsProperty( name = "IdToken" )
     @JsNullable
     public native String IdToken();
 
     @JsProperty
-    public native void setIdToken(@JsNullable String IdToken);
+    public native void setIdToken( @JsNullable String IdToken );
 
-    @JsProperty(name = "timeSkew")
+    @JsProperty( name = "timeSkew" )
     @JsNullable
     public native int timeSkew();
 
     @JsProperty
-    public native void setTimeSkew(int timeSkew);
+    public native void setTimeSkew( int timeSkew );
 
-    @JsProperty(name = "checkLoginIframe")
+    @JsProperty( name = "checkLoginIframe" )
     public native boolean checkLoginIframe();
 
     @JsProperty
-    public native void setCheckLoginIframe(boolean checkLoginIframe);
+    public native void setCheckLoginIframe( boolean checkLoginIframe );
 
-    @JsProperty(name = "checkLoginIframeInterval")
+    @JsProperty( name = "checkLoginIframeInterval" )
     public native int checkLoginIframeInterval();
 
     @JsProperty
-    public native void setCheckLoginIframeInterval(int checkLoginIframeIntervalInSeconds);
+    public native void setCheckLoginIframeInterval( int checkLoginIframeIntervalInSeconds );
 
-    @JsProperty(name = "responseMode")
+    @JsProperty( name = "responseMode" )
     @JsNonNull
     public native String responseMode();
 
     @JsProperty
-    public native void setResponseMode(@JsNonNull String responseMode);
+    public native void setResponseMode( @JsNonNull String responseMode );
 
-    @JsProperty(name = "flow")
+    @JsProperty( name = "flow" )
     @JsNonNull
     public native String flow();
 
     @JsProperty
-    public native void setFlow(@JsNonNull String responseMode);
+    public native void setFlow( @JsNonNull String responseMode );
 }
