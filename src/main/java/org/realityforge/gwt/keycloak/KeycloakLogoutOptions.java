17a02fcc3dd16490291c0914627a9234041a24cc
Peter Donald <peter@realityforge.org>
2024-06-28 09:54:26 +1000

    Grammar

--- src/main/java/org/realityforge/gwt/keycloak/KeycloakLogoutOptions.java
+++ src/main/java/org/realityforge/gwt/keycloak/KeycloakLogoutOptions.java
@@ -6,7 +6,7 @@ import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 /**
- * Options class to pass to logout action.
+ * Options class to pass to the logout action.
  */
 @JsType( isNative = true, namespace = JsPackage.GLOBAL, name = "Object" )
 public class KeycloakLogoutOptions
