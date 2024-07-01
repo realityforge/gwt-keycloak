d9464e7487cc1f9dea6b3c863524b76476f4d901
Peter Donald <peter@realityforge.org>
2024-07-02 08:56:45 +1000

    Whitespace

--- src/main/java/org/realityforge/gwt/keycloak/UpdateResponse.java
+++ src/main/java/org/realityforge/gwt/keycloak/UpdateResponse.java
@@ -1,13 +1,13 @@
 package org.realityforge.gwt.keycloak;
 
+import javax.annotation.Nullable;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
-
-import javax.annotation.Nullable;
 
 @JsType( isNative = true, namespace = JsPackage.GLOBAL )
 public class UpdateResponse
 {
-    public native UpdateResponse success( @Nullable KeycloakCallback successCallback );
-    public native UpdateResponse error( @Nullable KeycloakCallback failureCallback );
+  public native UpdateResponse success( @Nullable KeycloakCallback successCallback );
+
+  public native UpdateResponse error( @Nullable KeycloakCallback failureCallback );
 }
